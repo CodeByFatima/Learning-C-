17c2be0497a796b32b315aa9b900aa0025900bbf
CodeByFatima <fatimamadeit@gmail.com>
2026-03-05 04:54:52 +0500

    starting variables

---
+++ myfirstprogram.cpp
@@ -0,0 +1,93 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+
+// Another way to write the same code
+
+// #include <iostream>
+
+// int main() {
+//   std::cout << "Fatima here!";
+//   std::cout << "ALulu here!";
+//   return 0;
+// }
+
+// Challenge: Create a Simple C++ Program
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello, C++!";
+//     return 0;
+// }
+
+// Printing numbers
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << 17;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "I am ";
+//     cout << 17;
+//     return 0;
+// }
+
+// Performing calculations
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << 17 + 1;
+//     cout << 17 - 1;
+//     cout << 17 * 1;
+//     cout << 16 / 2;
+//     return 0;
+// }
+
+// Using next line
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World!\n\n";
+//     cout << "I am learning C++\n";
+//     cout << "Hello World!\n";
+//     cout << "I am learning C++\n";
+//     cout << "Hello World!" << endl;
+//     cout << "I am learning C++";
+//     return 0;
+// }
+
+// tab
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World!\t";
+//     cout << "ftm";
+//     return 0;
+// }
+
+// Challenge: Print Name and Age
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "fatima\n";
+   cout << 17;
+
+  return 0;
+}
